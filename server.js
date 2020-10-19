a9a75b0acfecb337d643922a154a6d7c952bcd2b
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-19 15:35:34 -0500

    initial commit with backend

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+
+app.use(express.static(__dirname+"/dist"));
+app.use(bodyParser.json());
+
+app.get('*', (req, res)=> {
+    res.sendFile('/dist/index.html', {root: __dirname + '/'});
+});
