d4b987943d016e1914f4d9d43fca4a75b12ced53
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-20 10:57:30 -0500

    finishes routes

--- server/routes/items.routes.js
+++ server/routes/items.routes.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const itemModels = require('../models/items.models');
 
+// Add Item
+router.post("/add", passport.authenticate('jwt', {session: false}), (req, res) => {
+    // check for valid info
+    if(!req.body.name || !req.body.qty || !req.body.price){
+        return res.send({success: false, msg: "Invalid values provided"})
+    }
 
+    // pass it to the model
+    itemModels.addItem(res, req.user.id, req.body.name, req.body.qty, req.body.price);
+
+})
+
+// Delete Item
+router.delete("/delete/:id", passport.authenticate('jwt', {session: false}), (req, res) => {
+    itemModels.removeItem(res, req.user.id, req.params.id);
+})
+
+// Get Items by User
+router.get("/user", passport.authenticate('jwt', {session: false}), (req, res) => {
+    itemModels.itemsByUser(res, req.user.id);
+})
 
 module.exports = router;
