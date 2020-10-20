e387aa1a57f9c9404741b822ccf4a585d828e56f
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-20 10:18:09 -0500

    basic item model layout

---
+++ server/models/items.models.js
@@ -0,0 +1,16 @@
+const pool = require("pool");
+const { addSyntheticTrailingComment } = require("typescript");
+
+// Add item
+function addItem(){}
+
+// Remove item
+function removeItem(){}
+
+// Items by user
+function itemsByUser(){}
+
+
+module.exports.addItem = addItem;
+module.exports.removeItem = removeItem;
+module.exports.itemsByUser = itemsByUser;
