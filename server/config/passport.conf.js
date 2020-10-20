942a8c8904842bebaa2282a44d4337c506ad6dba
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-20 11:18:17 -0500

    fixes typo

--- server/config/passport.conf.js
+++ server/config/passport.conf.js
@@ -1,4 +1,4 @@
-const pool = require("./mysql.config");
+const pool = require("../config/mysql.conf");
 const bcrypt = require("bcrypt");
 const JwtStrategy = require("passport-jwt").Strategy;
 const LocalStrategy = require("passport-local").Strategy;
@@ -14,17 +14,17 @@ function config(passport){
     passport.use('local-signup', new LocalStrategy({}, (username, password, done)=>{
         // Is username in use?
         pool.query("SELECT * FROM users WHERE users.username= ?", [username], (err, users)=>{
-            if(err) done(err, false, "Something went wrong. Try again later.");
+            if(err) return done(err, false, "Something went wrong. Try again later.");
 
             // If it's in use, tell them that it already exists
-            if (users[0]) done(null, false, "Username already in use");
+            if (users[0]) return done(null, false, "Username already in use");
 
             bcrypt.hash(passport, 8, (err, encrypted)=>{
-                if(err) done(err, false, "Something went wrong. Try again later");
+                if(err) return done(err, false, "Something went wrong. Try again later");
 
                 pool.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, encrypted], (err)=>{
-                    if(err) done(err, false, "Something went wrong. Try again later");
-                    done(null, {username: username}, "Successfully signed up, please log in!");
+                    if(err) return done(err, false, "Something went wrong. Try again later");
+                    return done(null, {username: username}, "Successfully signed up, please log in!");
                 })
             })
         })
@@ -35,9 +35,9 @@ function config(passport){
     passport.use('local-login', new LocalStrategy({}, (username, password, done)=>{
         // Grab user by username they provide
         pool.query("SELECT * FROM users WHERE users.username= ?", [username], (err, users)=>{
-            if(err) done(err, false, "Something went wrong. Try again later.");
+            if(err) return done(err, false, "Something went wrong. Try again later.");
 
-            if(!users[0]) done(null, false, "Invalid username or password");
+            if(!users[0]) return done(null, false, "Invalid username or password");
 
         
             // Check passwords
@@ -53,7 +53,7 @@ function config(passport){
 
 
     // JWT
-    passport.use('jwt', new JwtStrategy(opts, (jwt_payload, done)=>{
+    passport.use('jwt', new JwtStrategy(opt, (jwt_payload, done)=>{
         pool.query("SELECT * FROM users WHERE users.id = ?", [jwt_payload.id], (err, users)=>{
             if(err) done(err, false);
 
