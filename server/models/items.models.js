dec098f112dcac136a487cfa72d23e7b84734860
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-20 10:30:34 -0500

    adds functionality to items module

--- server/models/items.models.js
+++ server/models/items.models.js
@@ -2,13 +2,36 @@ const pool = require("pool");
 const { addSyntheticTrailingComment } = require("typescript");
 
 // Add item
-function addItem(){}
+function addItem(res, userId, name, qty, price){
+    // try to add item
+    pool.query("INSERT INTO items (userId, name, qty, price) VALUE (?,?,?,?)", [userId, name, qty, price], (err)=>{
+        // send appropriate response
+        if(err) return res.send({success: false, msg: 'Something went wrong, try again later'});
+        return res.send({ success: true, msg: 'Item successfully added' });
+    })
+}
 
 // Remove item
-function removeItem(){}
+function removeItem(res, userId, itemId){
+    // try to remove item
+    pool.query("DELETE FROM items WHERE items.userID = ? AND items.id = ?", [userId, itemId], (err)=>{
+        // send appropriate response
+        if(err) return res.send({success: false, msg: 'Something went wrong, try again later'});
+        return res.send({ success: true, msg: 'Item successfully deleted' });
+    })
+
+
+}
 
 // Items by user
-function itemsByUser(){}
+function itemsByUser(res, userId){
+    // try to get items based off userId
+    pool.query("SELECT * FROM items WHERE items.userId = ?", [userId], (err, items)=> {
+        // send appropriate response
+        if(err) return res.send({success: false, msg: 'Something went wrong, try again later'});
+        return res.send({ success: true, msg: 'Item successfully returned', items: items });
+    })
+}
 
 
 module.exports.addItem = addItem;
