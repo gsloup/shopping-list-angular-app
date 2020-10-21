177d547e028fcb579d6b6c541e5742a6b0dbdfe6
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-21 09:42:20 -0500

    add list component information

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../interfaces/item.interface';
+import { ItemsService } from '../services/items.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+  items: Item[] = []
+  name = '';
+  qty: number = 0;
+  price: number = 0;
+
+  constructor(private itemService: ItemsService) { 
+    this.itemService.items$.subscribe(v => this.items = v)
+  }
 
   ngOnInit(): void {
+  }
+
+  addItem(){
+    this.itemService.addItem(this.name, this.qty, this.price);
   }
 
 }
