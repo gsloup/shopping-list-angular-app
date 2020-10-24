7a90527e74cb6f0f1bdc973455939f93512652a9
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-23 22:06:27 -0500

    adds guards

--- src/app/guards/generic.guard.ts
+++ src/app/guards/generic.guard.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenericGuard implements CanActivate {
+
+  user: string; // will determine via a boolean whether a user is signed in
+
+  constructor(private userService: UserService, private router: Router){
+    this.userService.user$.subscribe(user => this.user = user ? user.username: null)
+    console.log(this.user);
+    
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      // If a user is not signed in, they will be allowed to access '/login' or '/signup'
+      if (this.user === null) {
+        return true;
+      }
+      // Will not let signed in users access '/login' or '/signup'
+      this.router.navigate(['/shopping-list']);
+      return false;
   }
   
 }
--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+
+  user: string; // will determine via a boolean whether a user is signed in
+
+  constructor(private userService: UserService, private router: Router){
+    this.userService.user$.subscribe(user => this.user = user ? user.username: null)
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      
+      // Route an un-signed user to the login page
+      if (this.user === null) {
+        this.router.navigate(['/login'])
+        return false;
+      }
+
+      // Signed in users can access '/shopping-list'
+      if(state.url === '/shopping-list') {
+        return true;
+      }
+    
+      return true;
   }
   
 }
